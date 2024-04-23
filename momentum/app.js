7432a996a47f1bbe757b5af126cefba717713ed5
takiJEONG <jht3267@gmail.com>
2024-04-23 17:24:02 +0900

    Update app.js

--- momentum/app.js
+++ momentum/app.js
@@ -1,12 +1,9 @@
-const mon ="mon";
-const tue = "tue";
-const wed = "wed";
-const thu = "thu";
-const fri = "fri";
-const sat = "sat";
-const sun = "sun";
+const daysOfWeek = ["mon" , "tue" , "wed" , "thu" , "fri" , "sat" , "sun"];
 
-const daysOfWeek = [mon , tue , wed , thu , fri , sat , sun];
+// Get Item from Array
+console. log(daysOfWeek);
 
-const nonsense = [1, 2, "Welcome", false, null, true, undefined, "Tak"]
-console. log(daysOfWeek,nonsense)
+// Add one more day to the array
+daysOfWeek.push("sun");
+
+console. log(daysOfWeek);
