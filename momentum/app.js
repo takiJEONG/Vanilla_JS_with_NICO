64bfad69fc98b59df74a65d278a897524d1381c8
takiJEONG <jht3267@gmail.com>
2024-04-23 15:58:40 +0900

    Update app.js
    blooean : true와 false에는 ""붙이지 않는다. 붙이면 string이 되는 거니까.

--- momentum/app.js
+++ momentum/app.js
@@ -1,11 +1,2 @@
-let a = 5;
-let b = 2;
-let myName = "HongTak" 
-
-console. log(5 + b);
-console. log(5 * b);
-console. log(5 / b);
-
-myName = "JHT"
-
-console. log("your new name is " + myName)
+const amIFat = true;
+const amISlim = false;
