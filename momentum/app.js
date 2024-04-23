825b52f79333fdfcd8adfe695bde85183eda5dfe
takiJEONG <jht3267@gmail.com>
2024-04-23 17:53:57 +0900

    Update app.js
    learn about object

--- momentum/app.js
+++ momentum/app.js
@@ -1,9 +1,8 @@
-const daysOfWeek = ["mon" , "tue" , "wed" , "thu" , "fri" , "sat" , "sun"];
-
-// Get Item from Array
-console. log(daysOfWeek);
-
-// Add one more day to the array
-daysOfWeek.push("sun");
-
-console. log(daysOfWeek);
+const player = {
+    name: "HongTak",
+    points: 10,
+    fat: true,
+};
+console.log(player);
+console.log(player.name);
+player.name
