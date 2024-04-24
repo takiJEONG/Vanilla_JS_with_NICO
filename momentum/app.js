460536f9136aad69e5ca3084ae445c6629f28664
takiJEONG <jht3267@gmail.com>
2024-04-24 21:31:32 +0900

    Update app.js
    learn about Returns

--- momentum/app.js
+++ momentum/app.js
@@ -1,23 +1,8 @@
-const calculator = {
-    add: function(a, b){
-        console.log(a + b);
-    },
-    minus:function (a, b) {
-        console.log(a - b)
-    },
-    div:function (a, b) {
-        console.log (a / b);
-    },
-    multi: function (a, b) {
-        console.log (a * b);
-    },
-    power: function (a, b) {
-        console.log (a ** b);
-    },
-};
+const age = 96;
+function calculateKrAge(ageOfForeigner){
+    ageOfForeigner + 2;
+}
 
-calculator.add(5, 1);
-calculator.minus(30, 6);
-calculator.div(60, 5);
-calculator.multi(13, 2);
-calculator.power(3, 9);
+const KrAge = calculateKrAge(age);
+
+console.log(KrAge)
