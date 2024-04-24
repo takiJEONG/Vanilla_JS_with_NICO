227da30a51526c89cdc9cd3c6b234e8257d636b0
takiJEONG <jht3267@gmail.com>
2024-04-24 18:51:48 +0900

    Calculator challenge
    #2.10 RECAP II
    NICO's Challenge.
    Make own Calculator

--- momentum/app.js
+++ momentum/app.js
@@ -1,5 +1,23 @@
-function plus(a, b) {
-    console.log(a + b);
-}
+const calculator = {
+    add: function(a, b){
+        console.log(a + b);
+    },
+    minus:function (a, b) {
+        console.log(a - b)
+    },
+    div:function (a, b) {
+        console.log (a / b);
+    },
+    multi: function (a, b) {
+        console.log (a * b);
+    },
+    power: function (a, b) {
+        console.log (a ** b);
+    },
+};
 
-plus(5, 10);
+calculator.add(5, 1);
+calculator.minus(30, 6);
+calculator.div(60, 5);
+calculator.multi(13, 2);
+calculator.power(3, 9);
