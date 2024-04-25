49b40a1460930a01d9c3468bb968b5112bedc12e
takiJEONG <jht3267@gmail.com>
2024-04-25 17:35:12 +0900

    Update app.js
    When click click me, then it turns blue.

--- momentum/app.js
+++ momentum/app.js
@@ -1,7 +1,7 @@
 const title = document.querySelector(".hello h1:first-child");
 
 function handleTitleClick() {
-    console.log("title was clicked")
+    title.style.color = "blue";
 }
 
 title.addEventListener("click", handleTitleClick);
