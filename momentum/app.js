b524d9e75333ea2e37129b0415f3db073b9b5bac
takiJEONG <jht3267@gmail.com>
2024-04-26 14:21:13 +0900

    updates.
    using css and checking how it works.

--- momentum/app.js
+++ momentum/app.js
@@ -1,14 +1,7 @@
 const h1= document.querySelector(".hello h1:first-child");
 
 function handleTitleClick() {
-    const currentColor = h1.style.color;
-    let newColor;
-    if(currentColor === "blue"){
-        newColor = "tomato";
-    } else {
-        newColor = "blue";
-    }
-    h1.style.color = newColor;
+ h1.className = "active";
 }
 
 
