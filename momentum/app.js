fd921396d41d16ce230d4ce4d814b128244d8528
takiJEONG <jht3267@gmail.com>
2024-04-26 11:45:14 +0900

    0426
    Running if,else function.

--- momentum/app.js
+++ momentum/app.js
@@ -1,38 +1,12 @@
 const h1= document.querySelector(".hello h1:first-child");
 
 function handleTitleClick() {
-    h1.style.color = "blue";
+    if(h1.style.color === "blue"){
+        h1.style.color = "tomato";
+    } else {
+        h1.style.color = "blue";
+    }
 }
 
-function handleMouseEnter(){
-    h1.innerText = "Mouse is here!";
-}
-
-function handleMouseLeave(){
-    h1.innerText = "Mouse is gone!";
-}
-
-function handlewindowResize() {
-    document.body.style.backgroundColor = "tomato";
-}
-
-function handlewindowCopy() {
-    alert("copier! ARE YOU CHEATING?");
-}
-
-function handleWindowOffline() {
-    alert("SOS!! No WiFi!!!")
-}
-
-function handleWindowOnline() {
-    alert("ALL GOOD");
-}
 
 h1.addEventListener("click", handleTitleClick);
-h1.addEventListener("mouseenter", handleMouseEnter);
-h1.addEventListener("mouseleave", handleMouseLeave);
-
-window.addEventListener("resize", handlewindowResize);
-window.addEventListener("copy", handlewindowCopy);
-window.addEventListener("offline", handleWindowOffline);
-window.addEventListener("online", handleWindowOnline);
