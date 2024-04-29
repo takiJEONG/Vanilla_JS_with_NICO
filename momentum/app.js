278571913e5138894b775605c052aecd6b40a40b
takiJEONG <jht3267@gmail.com>
2024-04-29 12:01:38 +0900

    Update app.js

--- momentum/app.js
+++ momentum/app.js
@@ -1,4 +1,3 @@
-const loginform = document.querySelector("#login-form");
-const loginInput = loginForm.querySelector("input");
-const loginButton = loginForm.querySelector("button");
+const loginInput = document.querySelector("#login-form input");
+const loginButton = document.querySelector("login-form button");
 
