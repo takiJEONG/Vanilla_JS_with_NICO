90c8c4e85b59a90dde97f899a64cd549b117781b
takiJEONG <jht3267@gmail.com>
2024-04-30 17:57:09 +0900

    Update app.js

--- momentum/app.js
+++ momentum/app.js
@@ -2,11 +2,14 @@ const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 
+const HIDDEN_CLASSNAME = "hidden";
+
 function onLoginSubmit(event) {
     event.preventDefault();
-    loginForm.classList.add("hidden");
+    loginForm.classList.add(HIDDEN_CLASSNAME);
   const username = loginInput.value;
   greeting.innerText = "Hello " + username;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
 
