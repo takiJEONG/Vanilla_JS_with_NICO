32648cafb27f963180c2c4c3819a9d762305c0c2
takiJEONG <jht3267@gmail.com>
2024-05-01 12:10:23 +0900

    Update clock.js
    still creating

--- momentum/js/clock.js
+++ momentum/js/clock.js
@@ -1,8 +1,9 @@
 const clock = document.querySelector("h2#clock");
 
-function sayHello() {
-    console.log("hello");
+function getClock() {
+    const date = new Date();
+    console.log(`${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`);
 }
 
 
-setInterval(sayHello, 5000);
+setInterval(getClock, 1000);
