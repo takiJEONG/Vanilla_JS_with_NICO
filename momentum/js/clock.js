33e29c6e15a877dba6d4aa74e510587b66318612
takiJEONG <jht3267@gmail.com>
2024-05-01 16:14:37 +0900

    Update clock.js

--- momentum/js/clock.js
+++ momentum/js/clock.js
@@ -2,6 +2,9 @@ const clock = document.querySelector("h2#clock");
 
 function getClock() {
     const date = new Date();
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+    const seconds = date.getSeconds();
     clock.innerText = (`${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`);
 }
 
