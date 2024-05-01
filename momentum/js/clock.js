e89984892f1f5d7bca8dd0c085313fa2f5fbafb8
takiJEONG <jht3267@gmail.com>
2024-05-01 15:54:43 +0900

    Update clock.js
    Finished to make a clock

--- momentum/js/clock.js
+++ momentum/js/clock.js
@@ -2,8 +2,8 @@ const clock = document.querySelector("h2#clock");
 
 function getClock() {
     const date = new Date();
-    console.log(`${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`);
+    clock.innerText = (`${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`);
 }
 
-
+getClock();
 setInterval(getClock, 1000);
