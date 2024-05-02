e7925965518ec054fe68d092dcc3313c03b4a785
takiJEONG <jht3267@gmail.com>
2024-05-02 16:49:03 +0900

    update
    Finished creating show random Background Images

--- momentum/js/background.js
+++ momentum/js/background.js
@@ -5,6 +5,15 @@ const images = [
     "3.jpeg",
     "4.jpeg",
     "5.jpeg",
+    "6.jpeg",
+    "7.jpeg",
+    "8.jpeg",
+    "9.jpeg",
+    "10.jpeg",
+    "11.jpeg",
+    "12.jpeg",
+    "13.jpeg",
+    "14.jpeg",
 ];
 
 const chosenImage = images[Math.floor(Math.random() * images.length)]
@@ -13,4 +22,4 @@ const bgImage = document.createElement("img");
 
 bgImage.src = `img/${chosenImage}`;
 
-console.log(bgImage);
+document.body.appendChild(bgImage);
