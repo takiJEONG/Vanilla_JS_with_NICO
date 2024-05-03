848b5df5ceb9bfcf088d7d67e428207d1f8bc2f8
takiJEONG <jht3267@gmail.com>
2024-05-03 11:40:02 +0900

    update
    creating todo.js. ...

---
+++ momentum/js/todo.js
@@ -0,0 +1,10 @@
+const toDoForm = document.getElementById("todo-form");
+const toDoInput = toDoForm.querySelector("input");
+const toDoList = document.getElementById("todo-list");
+
+
+function handleToDoSubmit(event) {
+    event.preventDefault();
+}
+
+toDoForm.addEventListener("submit", handleToDoSubmit);
