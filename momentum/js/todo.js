24b14ec6beafb4cbd62b9ccf3c6f5e0e7de49cc4
takiJEONG <jht3267@gmail.com>
2024-05-03 17:12:17 +0900

    Update todo.js

--- momentum/js/todo.js
+++ momentum/js/todo.js
@@ -2,14 +2,21 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
 
+
+function deleteToDo() {
+
+}
+
+
 function paintToDo(newTodo) {
   const li = document.createElement("li")
   const span = document.createElement("span");
+  span.innerText = newTodo;
   const button =document.createElement("button");
   button.innerText = "❌";
+  button.addEventListener("click", deleteToDo);
   li.appendChild(span);
   li.appendChild(button);
-  span.innerText = newTodo;
   toDoList.appendChild(li);
 }
 
