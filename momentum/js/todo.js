60151f226789800d0b9997e1eb6e1af9ee5b4581
takiJEONG <jht3267@gmail.com>
2024-05-03 16:32:48 +0900

    update

--- momentum/js/todo.js
+++ momentum/js/todo.js
@@ -1,10 +1,16 @@
 const toDoForm = document.getElementById("todo-form");
-const toDoInput = toDoForm.querySelector("input");
+const toDoInput = toDoForm.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
 
+function paintToDo(newTodo) {
+  const li = document.createElement("li")
+}
 
 function handleToDoSubmit(event) {
     event.preventDefault();
+    const newTodo = toDoInput.value;
+    toDoInput.value = "";
+    paintToDo(newTodo)
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
